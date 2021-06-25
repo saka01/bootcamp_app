b4dcdf979fd025fc32a181b785176ca7aecdaf59
Saka <sakamoshood@ymail.com>
2021-06-24 21:45:11 -0400

    upgrade pool.query

--- teachers.js
+++ teachers.js
@@ -7,19 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-});
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.name}`);
+    })
+  });
